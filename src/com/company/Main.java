62df982853e4f0ce09d0d455b6096497ebbd4482
ivo4ka113 <ivo4ka@gmail.com>
2015-10-08 22:32:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a1 = {5, 3, 6, 9, 8, 0, 4, 3, 1, 2};
+        int[] a2 = {5, 3, 6, 9, 8, 0, 4, 3, 1, 2};
+        int[] a3 = {5, 3, 6, 9, 8, 0, 4, 3, 1, 2};
+        int[] a4 = {5, 3, 6, 9, 8, 0, 4, 3, 1, 2};
+        int[] a5 = {5, 3, 6, 9, 8, 0, 4, 3, 1, 2};
+        for (int i = 0; i < a1.length; i++) {
+            System.out.print(a1[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < a2.length; i++) {
+            System.out.print(a1[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < a3.length; i++) {
+            System.out.print(a1[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < a4.length; i++) {
+            System.out.print(a1[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < a5.length; i++) {
+            System.out.print(a1[i] + " ");
+        }
+    }
+}
